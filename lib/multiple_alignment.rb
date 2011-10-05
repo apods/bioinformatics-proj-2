877167ab8e51cc34daad66a482ce58012950c32a
Gino Sanzi <eugene.sanzi@gmail.com>
2011-10-04 19:21:50 -0700

    Project is now bug free and 100% working

--- lib/multiple_alignment.rb
+++ lib/multiple_alignment.rb
@@ -32,47 +32,18 @@ class MultipleAlignment
   def next_alignment
   
     mg = ManhattanGraph.new(@s_center_string, @as_strands[0])
-    #mg = ManhattanGraph.new(@as_align[0], @as_strands[0])
     a_pair_alignment = mg.alignment
     
-    
-    
-    for i in 0 .. max(Array.[](a_pair_alignment[0].size, @as_align[0].size)) - 1
+    i = 0
+    until @as_align[0].size == a_pair_alignment[0].size
       if @as_align[0][i].chr == "-" and a_pair_alignment[0][i] != @as_align[0][i]
         a_pair_alignment[0].insert(i, "-")
         a_pair_alignment[1].insert(i, "-")
       elsif a_pair_alignment[0][i].chr == "-" and a_pair_alignment[0][i] != @as_align[0][i]
         space_insert(i)
       end
+      i += 1
     end
-
-
-    # Align old string to new
-    # Align new string to old
-
-=begin   
-    # If a space was inserted into the center string, check to see if it needs
-    # to be inserted into the multiple alignment as well
-    for i in 0 .. a_pair_alignment[0].size - 1
-      space_insert(i) unless ((a_pair_alignment[0][i].chr != "-") or (@as_align[0][i].chr == "-"))
-    end
-    #############################################################
-    # Add spaces to the string just aligned if necessary in order to keep it
-    # Aligned with the center string when inserted
-    counter_i = 0
-    space_additions = Array.new
-    for j in 0 .. a_pair_alignment[1].size - 1
-      until a_pair_alignment[0][j] == @as_align[0][counter_i]
-        space_additions << counter_i
-        counter_i += 1
-      end
-      
-      counter_i += 1
-    end
-    space_additions.each { |idx| a_pair_alignment[1].insert(idx, "-") }
-    
-    ###########################################################
-=end
 
     append(a_pair_alignment[1])
     @as_strands.delete_at(0)
